3d99eda9096de927cc621b50807d1de71aaf7a68
vaghanikrisha <155968108+vaghanikrisha@users.noreply.github.com>
2024-02-21 14:08:57 +0530

    program 23.2.c

--- C lecture 23/program 23.2.c
+++ C lecture 23/program 23.2.c
@@ -7,7 +7,9 @@ main()
     scanf("%d", &A);
     printf("Enter the value of B: ");
     scanf("%d", &B);
-    
+
+    printf("-----------------------------\n");
+	
     printf("Before swapping:\n");
     printf("A : %d\n", A);
     printf("B : %d\n", B);
@@ -18,7 +20,9 @@ main()
     C = *pointer1;
     *pointer1 = *pointer2;
     *pointer2 = C;
-    
+
+     printf("-----------------------------\n");
+	
     printf("After swapping:\n");
     printf("A : %d\n", A);
     printf("B : %d\n", B);
