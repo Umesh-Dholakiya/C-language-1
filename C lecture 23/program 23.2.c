5d96365903b24d6925111e19a6150ce6adce9a48
vaghanikrisha <155968108+vaghanikrisha@users.noreply.github.com>
2024-02-21 06:18:52 +0530

    Add files via upload

---
+++ C lecture 23/program 23.2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int A , B , C ;
+	
+	printf("Enter the value of A: ");
+    scanf("%d", &A);
+    printf("Enter the value of B: ");
+    scanf("%d", &B);
+    
+    printf("Before swapping:\n");
+    printf("A : %d\n", A);
+    printf("B : %d\n", B);
+    
+    int *pointer1 , *pointer2;
+    pointer1 =&A , pointer2 =&B;
+    
+    C = *pointer1;
+    *pointer1 = *pointer2;
+    *pointer2 = C;
+    
+    printf("After swapping:\n");
+    printf("A : %d\n", A);
+    printf("B : %d\n", B);
+}
